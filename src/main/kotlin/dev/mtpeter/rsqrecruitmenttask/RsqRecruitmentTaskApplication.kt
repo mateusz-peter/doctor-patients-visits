691e7b87ca9e5105b86a28c3b0bca3bc22c771e1
Mateusz Peter <mateusz.peter@mtpeter.dev>
2022-12-19 15:12:03 +0100

    spring boot init

---
+++ src/main/kotlin/dev/mtpeter/rsqrecruitmenttask/RsqRecruitmentTaskApplication.kt
@@ -0,0 +1,11 @@
+package dev.mtpeter.rsqrecruitmenttask
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RsqRecruitmentTaskApplication
+
+fun main(args: Array<String>) {
+	runApplication<RsqRecruitmentTaskApplication>(*args)
+}
---
+++ src/test/kotlin/dev/mtpeter/rsqrecruitmenttask/RsqRecruitmentTaskApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.mtpeter.rsqrecruitmenttask
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RsqRecruitmentTaskApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
