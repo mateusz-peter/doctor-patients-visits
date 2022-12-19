0111819dbeee64b7eb82821f2fd93f8195fcffdc
Mateusz Peter <mateusz.peter@mtpeter.dev>
2022-12-19 16:43:13 +0100

    add missing kotest dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,8 @@ repositories {
 extra["testcontainersVersion"] = "1.17.6"
 extra["kotestVersion"] = "5.5.4"
 extra["springmockkVersion"] = "4.0.0"
+ extra["kotest-extension-testcontainersVersion"] = "1.3.4"
+extra["kotest-extension-springVersion"] = "1.1.2"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
@@ -43,6 +45,10 @@ dependencies {
 	testImplementation("org.testcontainers:junit-jupiter")
 	testImplementation("org.testcontainers:postgresql")
 	testImplementation("org.testcontainers:r2dbc")
+	testImplementation("io.kotest:kotest-runner-junit5")
+	testImplementation("io.kotest:kotest-assertions-core")
+	testImplementation("io.kotest.extensions:kotest-extensions-spring:${property("kotest-extension-springVersion")}")
+	testImplementation("io.kotest.extensions:kotest-extensions-testcontainers:${property("kotest-extension-testcontainersVersion")}")
 	testImplementation("com.ninja-squad:springmockk:${property("springmockkVersion")}")
 }
 
