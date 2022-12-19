c6184daf4fb659acc4f5475b220c1193f3b6d05c
Mateusz Peter <mateusz.peter@mtpeter.dev>
2022-12-19 22:16:10 +0100

    add sample configuration to make dummy app running with local database

--- src/main/kotlin/dev/mtpeter/rsqrecruitmenttask/RsqRecruitmentTaskApplication.kt
+++ src/main/kotlin/dev/mtpeter/rsqrecruitmenttask/RsqRecruitmentTaskApplication.kt
@@ -2,8 +2,10 @@ package dev.mtpeter.rsqrecruitmenttask
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 
 @SpringBootApplication
+@EnableR2dbcRepositories
 class RsqRecruitmentTaskApplication
 
 fun main(args: Array<String>) {
