c3128eaa07001ee26bc40595c7f9aa608368f84b
Mateusz Peter <mateusz.peter@mtpeter.dev>
2022-12-23 01:46:14 +0100

    implementation for GET on /visits

--- src/main/kotlin/dev/mtpeter/rsqrecruitmenttask/visit/VisitRouter.kt
+++ src/main/kotlin/dev/mtpeter/rsqrecruitmenttask/visit/VisitRouter.kt
@@ -1,10 +1,13 @@
 package dev.mtpeter.rsqrecruitmenttask.visit
 
 import dev.mtpeter.rsqrecruitmenttask.configuration.TenantAwareRouting
+import kotlinx.coroutines.flow.Flow
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Component
-import org.springframework.web.reactive.function.server.buildAndAwait
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.bodyAndAwait
 import org.springframework.web.reactive.function.server.coRouter
 
 @Configuration
@@ -15,9 +18,7 @@ class VisitRouter {
         visitHandler: VisitHandler,
         tenantAwareRouting: TenantAwareRouting
     ) = coRouter {
-        GET("/visits") {
-            ok().buildAndAwait()
-        }
+        GET("/visits", visitHandler::getAllVisits)
 
         filter(tenantAwareRouting::tenantAwareFilter)
     }
@@ -27,12 +28,14 @@ class VisitRouter {
 class VisitHandler(
     private val visitService: VisitService
 ) {
-
+    suspend fun getAllVisits(serverRequest: ServerRequest): ServerResponse {
+        return ServerResponse.ok().bodyAndAwait(visitService.getAllVisits())
+    }
 }
 
 @Component
 class VisitService(
     private val visitRepository: VisitRepository
 ) {
-
+    fun getAllVisits(): Flow<Visit> = visitRepository.findAll()
 }
