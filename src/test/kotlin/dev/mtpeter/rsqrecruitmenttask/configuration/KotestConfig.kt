844c0a0e76913956401eaaef5289189e3abe9897
Mateusz Peter <mateusz.peter@mtpeter.dev>
2022-12-28 13:11:02 +0100

    move postgres testcontainer to kotest project config

--- src/test/kotlin/dev/mtpeter/rsqrecruitmenttask/configuration/KotestConfig.kt
+++ src/test/kotlin/dev/mtpeter/rsqrecruitmenttask/configuration/KotestConfig.kt
@@ -4,6 +4,9 @@ import io.kotest.core.config.AbstractProjectConfig
 import io.kotest.core.extensions.Extension
 import io.kotest.core.spec.IsolationMode
 import io.kotest.extensions.spring.SpringExtension
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.containers.wait.strategy.HostPortWaitStrategy
+import org.testcontainers.utility.MountableFile
 
 class KotestConfig : AbstractProjectConfig() {
 
@@ -11,4 +14,28 @@ class KotestConfig : AbstractProjectConfig() {
         get() = IsolationMode.InstancePerLeaf
 
     override fun extensions(): List<Extension> = listOf(SpringExtension)
+
+    override suspend fun beforeProject() {
+        super.beforeProject()
+
+        PostgreSQLContainer<Nothing>("postgres:15.1-alpine").run {
+            startupAttempts = 1
+            withCopyToContainer(
+                MountableFile.forClasspathResource("dbSchema.sql"),
+                "/docker-entrypoint-initdb.d/init.sql"
+            )
+            withUsername("test")
+            withPassword("test")
+            withDatabaseName("tenantA")
+            start()
+            waitingFor(HostPortWaitStrategy())
+
+            System.setProperty("dev.mtpeter.rsq.tenants.tenantA.username", this.username)
+            System.setProperty("dev.mtpeter.rsq.tenants.tenantA.password", this.password)
+            System.setProperty(
+                "dev.mtpeter.rsq.tenants.tenantA.url",
+                "r2dbc:postgresql://$host:${getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT)}/$databaseName"
+            )
+        }
+    }
 }
--- src/test/kotlin/dev/mtpeter/rsqrecruitmenttask/multitenant/MTIntegrationTest.kt
+++ src/test/kotlin/dev/mtpeter/rsqrecruitmenttask/multitenant/MTIntegrationTest.kt
@@ -4,13 +4,7 @@ import io.kotest.core.spec.style.BehaviorSpec
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.DynamicPropertyRegistry
-import org.springframework.test.context.DynamicPropertySource
 import org.springframework.test.web.reactive.server.WebTestClient
-import org.testcontainers.containers.PostgreSQLContainer
-import org.testcontainers.utility.MountableFile
-import kotlin.time.Duration.Companion.seconds
-import kotlin.time.toJavaDuration
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class MTIntegrationTest @Autowired constructor(
@@ -20,32 +14,6 @@ class MTIntegrationTest @Autowired constructor(
 
     val webTestClient = WebTestClient
         .bindToServer().baseUrl("http://localhost:${port}").build()
-        .mutate().responseTimeout(30.seconds.toJavaDuration()).build()
-
-    companion object {
-        val postgres = PostgreSQLContainer<Nothing>("postgres:15.1-alpine").apply {
-            startupAttempts = 1
-            withCopyToContainer(
-                MountableFile.forClasspathResource("dbSchema.sql"),
-                "/docker-entrypoint-initdb.d/init.sql"
-            )
-            withUsername("test")
-            withPassword("test")
-            withDatabaseName("tenantA")
-            start()
-        }
-
-        @JvmStatic
-        @DynamicPropertySource
-        fun properties(registry: DynamicPropertyRegistry) {
-            registry.add("dev.mtpeter.rsq.tenants.tenantA.username", postgres::getUsername)
-            registry.add("dev.mtpeter.rsq.tenants.tenantA.passowrd", postgres::getPassword)
-            registry.add("dev.mtpeter.rsq.tenants.tenantA.url", ::r2dbcUrl)
-        }
-
-        fun r2dbcUrl() = "r2dbc:postgresql://${postgres.host}:${postgres.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT)}/${postgres.databaseName}"
-    }
-
 
     init {
         this.given("GET Request on /patients") {
